b61bf930227b10af873db6f3a246b479c1e24b7a
Takuma HANATANI <mail@potato4d.me>
2021-07-28 15:43:47 +0900

    chore: Fix error in other's deck recipe

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { createApp } from '@vue/runtime-dom'
 import { App } from './app'
 
-const btnarea_cd = document.getElementById('btnarea_cd')!
-
+const app = createApp(App)
 const tameshiDrawRoot = document.createElement('div')
 tameshiDrawRoot.id = 'tameshiDrawRoot'
 
-btnarea_cd.prepend(tameshiDrawRoot)
+const btnarea_cd = document.getElementById('btnarea_cd')
 
-const app = createApp(App)
+if (btnarea_cd) {
+  btnarea_cd.prepend(tameshiDrawRoot)
+  app.mount('#tameshiDrawRoot')
+} else {
+  const appendTarget = document.querySelector('#deck_image')!.nextSibling!.parentElement!
+  appendTarget.append(tameshiDrawRoot)
+  app.mount('#tameshiDrawRoot')
+}
 
-app.mount('#tameshiDrawRoot')
